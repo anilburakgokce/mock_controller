696ce7fecf54b69f943d698fbbab9c181a12d7e3
Anıl Burak Gökce <anilburakgokce@gmail.com>
2026-01-13 16:15:50 +0100

    update device.cpp

--- device.cpp
+++ device.cpp
@@ -9,7 +9,7 @@ vector<uint16_t> Device::read_samples() {
     this_thread::sleep_for(chrono::microseconds(10)); // hw latency
 
     default_random_engine generator;
-    uniform_int_distribution<int> distribution(numeric_limits<uint16_t>::min(),numeric_limits<uint16_t>::max());
+    uniform_int_distribution<uint16_t> distribution(numeric_limits<uint16_t>::min(),numeric_limits<uint16_t>::max());
 
     vector<uint16_t> samples;
     for(int i = 0; i < 5; i++){
