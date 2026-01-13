20130771a9b1297ff21d3c4c1df5e340d729cd39
Anıl Burak Gökce <anilburakgokce@gmail.com>
2026-01-13 16:15:13 +0100

    update device.cpp

--- device.cpp
+++ device.cpp
@@ -3,11 +3,18 @@
 #include <thread>
 #include <chrono>
 
-using namespace std;
-
 Device::Device() {}
 
 vector<uint16_t> Device::read_samples() {
     this_thread::sleep_for(chrono::microseconds(10)); // hw latency
-    return {1, 2, 3, 4, 5};
+
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(numeric_limits<uint16_t>::min(),numeric_limits<uint16_t>::max());
+
+    vector<uint16_t> samples;
+    for(int i = 0; i < 5; i++){
+        samples.push_back(distribution(generator));
+    }
+
+    return samples;
 }
