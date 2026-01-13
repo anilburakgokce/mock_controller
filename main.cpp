1debacb939d88c0b629bca47cf9f9f5d3222111d
Anıl Burak Gökce <anilburakgokce@gmail.com>
2026-01-13 16:45:18 +0100

    added wait for joining the threads

--- main.cpp
+++ main.cpp
@@ -10,10 +10,14 @@ int main(){
 
     Controller ctrl;
     
-    for (int i = 0; i < num_threads; i++) {
+    for(int i = 0; i < num_threads; i++) {
         threads.emplace_back([&ctrl, i]() {
             ctrl.acquire(i);
         });
+    }
+
+    for(auto& t : threads) {
+        t.join();
     }
 
     return 0;
