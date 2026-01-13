f5aa61e4aa0d91bcd41065164c65d5679d2056e4
Anıl Burak Gökce <anilburakgokce@gmail.com>
2026-01-13 16:51:29 +0100

    device codebase refactor

--- device.cpp
+++ device.cpp
@@ -1,15 +1,13 @@
 #include "device.hpp"
-#include <random>
 #include <thread>
 #include <chrono>
 
-Device::Device() {}
+Device::Device() {
+    uniform_int_distribution<uint16_t> distribution(numeric_limits<uint16_t>::min(),numeric_limits<uint16_t>::max());
+}
 
 vector<uint16_t> Device::read_samples() {
     this_thread::sleep_for(chrono::microseconds(10)); // hw latency
-
-    default_random_engine generator;
-    uniform_int_distribution<uint16_t> distribution(numeric_limits<uint16_t>::min(),numeric_limits<uint16_t>::max());
 
     vector<uint16_t> samples;
     for(int i = 0; i < 5; i++){
--- device.hpp
+++ device.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <cstdint>
+#include <random>
 
 using namespace std;
 
@@ -8,4 +9,8 @@ class Device {
 public:
     Device();
     vector<uint16_t> read_samples();
+
+private:
+    default_random_engine generator;
+    uniform_int_distribution<uint16_t> distribution;
 };
